91363da5229b2931c35c592929465216fb3b865c
dishant <aryadishant414@gmail.com>
2024-06-24 17:59:08 +0530

    modified call.js

--- Classes and OOPs/call.js
+++ Classes and OOPs/call.js
@@ -5,7 +5,7 @@ function SetUsername(username){
 }
 
 function createUser(username, email, password){
-    SetUsername.call(this, username)
+    SetUsername.call(this, username)  // yaha par hamne optionally 'this' ko send kiya hai coz jab 'setUsername' method call hoga tab usme jo this hai uska scope too uska function khatam hone tak hee rahega uske baad too wo function cal stack se hatt jaaega islie hamne apna 'this' pass kiya hai so 'setUsernmae' method iss baar hamara wala 'this' lega jo hamne optionally pass kiya hai wo.
    
     this.email = email
     this.password = password
